a41c6205b1fb1a6f23f6ac54ffa1ae8a4db8ef4c
Daniel Ebert <daniel.ebert@protonmail.com>
2021-06-08 22:25:22 +0200

    Add packet.rs

---
+++ werewolf-rs/src/packet.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+
+pub fn serialize_packet<P: Serialize>(packet: &P) -> Result<String> {
+    let raw: String = serde_json::ser::to_string(packet)?;
+    Ok(raw)
+}
+
+pub fn deserialize_packet<'a, P: Deserialize<'a>>(raw: &'a str) -> Result<P> {
+    let packet: P = serde_json::from_str(raw)?;
+    Ok(packet)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum PacketToServer {
+    Pong(String),
+    Unknown,
+    CloseConnection
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum PacketToClient {
+    Ping(String),
+    CloseConnection
+}
